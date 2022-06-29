cbf639de6ebc3e11f013fcb83ce9f7249af86934
ctn-osmn94 <cetinosman676@gmail.com>
2022-06-29 13:45:28 +0300

    total price and delete item completed

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { ref } from 'vue'
 
 const amount = ref(0)
 const shopList = ref([])
-const addBasket = ref([])
+const Basket = ref([])
 
 function getItems() {
     fetch("/shop.json").then(res=>res.json())
@@ -14,6 +14,26 @@ function getItems() {
     })
 }
 getItems()
+
+function addBasket(item) {
+      amount.value++
+      Basket.value.push(item)
+}
+
+function totalPrice() {
+    return Basket.value.reduce((current,next)=>
+    current+next.price,0)
+}
+totalPrice()
+addBasket()
+function deleteItem(index) {
+      Basket.value.splice(index,1)
+      amount.value--
+      return Basket.value.reduce((current,next)=>
+      next-current.price,0)
+}
+deleteItem()
+
 
 
 </script>
@@ -29,10 +49,24 @@ getItems()
             <div class="flex items-center justify-between">
                 <p class="text-xl font-semibold"> {{item.name}} </p>
                 <p class="text-xl font-semibold"> {{item.price}} $ </p>
-                <button class="p-3 bg-green-200 rounded-md" @click="Basket(item)">basket
+                <button class="p-3 bg-green-200 rounded-md" @click="addBasket(item)">basket
                 
                 </button>
             </div>
+        </div>
+    </div>
+    <div class="container mx-auto ">
+        <h1 class="font-bold text-2xl mb-8">BASKET</h1>
+        <div class="flex flex-col gap-5">
+            
+            <div class="flex gap-3 items-center" v-for="(item,index) in Basket"
+            :key="item.id" >
+                <p> {{item.name}} </p>
+                <p> {{item.price}} $ </p>
+                <i @click="deleteItem(index)" class="fa-solid fa-trash-can cursor-pointer"></i>
+            </div>
+            <p> total amount: {{amount}} </p>
+            <p> total price: {{totalPrice()}} $ </p>
         </div>
     </div>
 </template>
